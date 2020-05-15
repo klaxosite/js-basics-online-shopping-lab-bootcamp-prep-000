919a85e827cafb7e9bc790d16311f86849ee9169
Jastien Sandhu <superjastin@gmail.com>
2020-05-15 02:04:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,30 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var totals = {itemName: item, itemPrice: Math.floor(Math.random()*100) }
+  cart.push(totals)
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if(cart.length == 0)
+  {
+    return "Your shopping cart is empty."
+  }else if (cart.length == 1) {
+
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`
+
+  }else if (cart.length == 2){
+    return `In your cart, you have ${cart[1].itemName} at $${cart[1].itemPrice}`
+  }else{
+
+  }
+
 }
 
+
 function total() {
-  // write your code here
+
 }
 
 function removeFromCart(item) {
@@ -26,5 +41,6 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+
+
 }
