29a781b75fe64f7f057b6a961ffa76b6cba8bb0c
Jastien Sandhu <superjastin@gmail.com>
2020-05-15 02:49:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function removeFromCart(item) {
   if(item != cart.itemName){
     return "That item is not in your cart."
   } else{
-    for (let i = 0; i < cart.length; i++)
+    for (let i = 0; i < item.cart.length; i++)
     {
       if (item == cart[i].itemName){
         cart.splice(i, 1)
