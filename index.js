86e45534ffe7d860c032746c523dd88d382cad26
Jastien Sandhu <superjastin@gmail.com>
2020-05-15 03:14:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,8 +63,9 @@ function placeOrder(cardNumber) {
   if(cardNumber === undefined){
     return "Sorry, we don't have a credit card on file for you."
   }
+  let totes = total()
     cart = []
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${totes}, which will be charged to the card ${cardNumber}.`
 
 
 
